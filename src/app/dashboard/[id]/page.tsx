f94def5be94146ff0b331cebe4b065987aa64fda
Zohaib Hassan <zhassan.ce44ceme@student.nust.edu.pk>
2025-07-06 16:50:07 +0500

    Enable dashboard page with room access control
    Uncommented and integrated Canvas and Room components. Added logic to fetch room details, check user access (owner or invitee), and redirect unauthorized users to 404. The page now renders the Canvas within the Room context, passing relevant room data.

--- src/app/dashboard/[id]/page.tsx
+++ src/app/dashboard/[id]/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from "next/navigation";
-// import Canvas from "@/components/canvas/Canvas";
-// import { Room } from "@/components/liveblocks/Room";
+import Canvas from "@/components/canvas/Canvas";
+import { Room } from "@/components/liveblocks/Room";
 import { auth } from "@/server/auth";
 import { db } from "@/server/db";
 
@@ -11,13 +11,36 @@ export default async function Page({ params }: { params: ParamsType }) {
 
   const session = await auth();
 
-  
-  return (
-    <></>
-    //   <Room>
-    //     <Canvas>
+  const room = await db.room.findUnique({
+    where: { id: id },
+    select: {
+      title: true,
+      ownerId: true,
+      roomInvites: {
+        select: {
+          user: true,
+        },
+      },
+    },
+  });
 
-    //     </Canvas>
-    //   </Room>
+  if (!room) redirect("/404");
+
+  const inviteeUserIds = room.roomInvites.map((invite) => invite.user.id);
+  if (
+    !inviteeUserIds.includes(session?.user.id ?? "") &&
+    session?.user.id !== room.ownerId
+  ) {
+    redirect("/404");
+  }
+
+  return (
+    <Room roomId={"room:" + id}>
+      <Canvas
+        roomName={room.title}
+        roomId={id}
+        othersWithAccessToRoom={room.roomInvites.map((x) => x.user)}
+      />
+    </Room>
   );
 }
