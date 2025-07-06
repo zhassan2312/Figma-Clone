a8d07a4f05beaddd940344e2e0d87dcef8cea3fc
Zohaib Hassan <zhassan.ce44ceme@student.nust.edu.pk>
2025-07-06 16:27:35 +0500

    Add type definitions for canvas and layers
    Introduces types for colors, camera, layers (rectangle, ellipse, path, text), points, and canvas state management. Also defines enums for layer types, canvas modes, and sides to support a drawing or design application.

---
+++ src/types.ts
@@ -0,0 +1,138 @@
+export type Color = {
+  r: number;
+  g: number;
+  b: number;
+};
+
+export type Camera = {
+  x: number;
+  y: number;
+  zoom: number;
+};
+
+export enum LayerType {
+  Rectangle,
+  Ellipse,
+  Path,
+  Text,
+}
+
+export type RectangleLayer = {
+  type: LayerType.Rectangle;
+  x: number;
+  y: number;
+  height: number;
+  width: number;
+  fill: Color;
+  stroke: Color;
+  opacity: number;
+  cornerRadius?: number;
+};
+
+export type EllipseLayer = {
+  type: LayerType.Ellipse;
+  x: number;
+  y: number;
+  height: number;
+  width: number;
+  fill: Color;
+  stroke: Color;
+  opacity: number;
+};
+
+export type PathLayer = {
+  type: LayerType.Path;
+  x: number;
+  y: number;
+  height: number;
+  width: number;
+  fill: Color;
+  stroke: Color;
+  opacity: number;
+  points: number[][];
+};
+
+export type TextLayer = {
+  type: LayerType.Text;
+  x: number;
+  y: number;
+  height: number;
+  width: number;
+  text: string;
+  fontSize: number;
+  fontWeight: number;
+  fontFamily: string;
+  fill: Color;
+  stroke: Color;
+  opacity: number;
+};
+
+export type Layer = RectangleLayer | EllipseLayer | PathLayer | TextLayer;
+
+export type Point = {
+  x: number;
+  y: number;
+};
+
+export type XYWH = {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+};
+
+export enum Side {
+  Top = 1,
+  Bottom = 2,
+  Left = 4,
+  Right = 8,
+}
+
+export type CanvasState =
+  | {
+      mode: CanvasMode.None;
+    }
+  | {
+      mode: CanvasMode.RightClick;
+    }
+  | {
+      mode: CanvasMode.SelectionNet;
+      origin: Point;
+      current?: Point;
+    }
+  | {
+      mode: CanvasMode.Dragging;
+      origin: Point | null;
+    }
+  | {
+      mode: CanvasMode.Inserting;
+      layerType: LayerType.Rectangle | LayerType.Ellipse | LayerType.Text;
+    }
+  | {
+      mode: CanvasMode.Pencil;
+    }
+  | {
+      mode: CanvasMode.Resizing;
+      initialBounds: XYWH;
+      corner: Side;
+    }
+  | {
+      mode: CanvasMode.Translating;
+      current: Point;
+    }
+  | {
+      mode: CanvasMode.Pressing;
+      origin: Point;
+    };
+
+export enum CanvasMode {
+  None,
+  Dragging,
+  Inserting,
+  Pencil,
+  Resizing,
+  Translating,
+  SelectionNet,
+  Pressing,
+  RightClick,
+}
