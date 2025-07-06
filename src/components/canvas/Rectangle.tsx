f234a95c33a34f50d5bb57f270d5b00611b9f240
Zohaib Hassan <zhassan.ce44ceme@student.nust.edu.pk>
2025-07-06 18:09:58 +0500

    Add Rectangle component for canvas rendering
    Introduces a new Rectangle component that renders rectangle layers on the canvas, supporting fill, stroke, opacity, and corner radius. Handles pointer events and uses utility functions for color conversion.

---
+++ src/components/canvas/Rectangle.tsx
@@ -0,0 +1,40 @@
+import { RectangleLayer } from "@/types";
+import { colorToCss } from "@/utils";
+
+export default function Rectangle({
+  id,
+  layer,
+  onPointerDown,
+}: {
+  id: string;
+  layer: RectangleLayer;
+  onPointerDown: (e: React.PointerEvent, layerId: string) => void;
+}) {
+  const { x, y, width, height, fill, stroke, opacity, cornerRadius } = layer;
+
+  return (
+    <g className="group">
+      <rect
+        style={{ transform: `translate(${x}px, ${y}px)` }}
+        width={width}
+        height={height}
+        fill="none"
+        stroke="#0b99ff"
+        strokeWidth="4"
+        className="pointer-events-none opacity-0 group-hover:opacity-100"
+      />
+      <rect
+        onPointerDown={(e) => onPointerDown(e, id)}
+        style={{ transform: `translate(${x}px, ${y}px)` }}
+        width={width}
+        height={height}
+        fill={fill ? colorToCss(fill) : "#CCC"}
+        strokeWidth={1}
+        stroke={stroke ? colorToCss(stroke) : "#CCC"}
+        opacity={`${opacity ?? 100}%`}
+        rx={cornerRadius ?? 0}
+        ry={cornerRadius ?? 0}
+      />
+    </g>
+  );
+}
