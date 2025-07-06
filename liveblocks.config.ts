98d31a0d1d43213465fe7f9ba6fdbd923a3afee8
Zohaib Hassan <zhassan.ce44ceme@student.nust.edu.pk>
2025-07-06 16:31:55 +0500

    Expand Liveblocks types for presence and storage
    Added new properties to the Presence and Storage interfaces, including selection, cursor, penColor, pencilDraft, roomColor, layers, and layerIds. These changes support richer real-time collaboration features and improved type safety.

--- liveblocks.config.ts
+++ liveblocks.config.ts
@@ -1,4 +1,8 @@
 // Define Liveblocks types for your application
+
+import { LiveList, LiveMap, LiveObject } from "@liveblocks/client";
+import { Color, Layer, Point } from "@/types";
+
 // https://liveblocks.io/docs/api-reference/liveblocks-react#Typing-your-data
 declare global {
   interface Liveblocks {
@@ -6,12 +10,19 @@ declare global {
     Presence: {
       // Example, real-time cursor coordinates
       // cursor: { x: number; y: number };
+      selection: string[];
+      cursor: Point | null;
+      penColor: Color | null;
+      pencilDraft: [x: number, y: number, pressure: number][] | null;
     };
 
     // The Storage tree for the room, for useMutation, useStorage, etc.
     Storage: {
       // Example, a conflict-free list
       // animals: LiveList<string>;
+      roomColor: Color | null;
+      layers: LiveMap<string, LiveObject<Layer>>;
+      layerIds: LiveList<string>;
     };
 
     // Custom user info set when authenticating with a secret key
@@ -19,16 +30,16 @@ declare global {
       id: string;
       info: {
         // Example properties, for useSelf, useUser, useOthers, etc.
-        // name: string;
+        name: string;
         // avatar: string;
       };
     };
 
     // Custom events, for useBroadcastEvent, useEventListener
     RoomEvent: {};
-      // Example has two events, using a union
-      // | { type: "PLAY" } 
-      // | { type: "REACTION"; emoji: "🔥" };
+    // Example has two events, using a union
+    // | { type: "PLAY" }
+    // | { type: "REACTION"; emoji: "🔥" };
 
     // Custom metadata set on threads, for useThreads, useCreateThread, etc.
     ThreadMetadata: {
